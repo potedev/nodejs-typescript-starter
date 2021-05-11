a72d5b85ce34a8e1b0cffa5c1ef55012d60c3f6c
potedev <quentin@pote.dev>
2021-05-11 15:42:55 +0200

    [WIP] Using TypeORM...

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
 import { createServer } from './app/http/app'
+import { createConnection } from 'typeorm'
+
+import { User } from './app/database/typeorm/entities/user'
+
+createConnection({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "",
+    database: "skilltree_dev",
+    synchronize: true,
+    logging: true,
+    entities: [User]
+}).then(conn => {
+    console.log('connection established');
+})
+
 
 createServer();

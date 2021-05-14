f7334686a436f322ad3c72c8072ad7f067e2304e
potedev <quentin@pote.dev>
2021-05-14 11:16:54 +0200

    [Entity][Relation] Fixed One to many between category & skill

--- src/app/database/typeorm/entities/category.ts
+++ src/app/database/typeorm/entities/category.ts
@@ -13,6 +13,6 @@ export class Category extends BaseEntity {
     @Column()
     description: string;
 
-    @OneToMany(() => Category, category => category.skills)
+    @OneToMany(() => Skill, skill => skill.category)
     skills: Skill[]
 }
